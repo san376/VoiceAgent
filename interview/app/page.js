d2c6e44fd1b8b7aac92c1ea9b27631cc0e6e0d93
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-04-08 23:58:56 +0530

    add supabase

--- interview/app/page.js
+++ interview/app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
      <h2>Hello</h2>
+     <Button>Finish</Button>
    </div>
   );
 }
