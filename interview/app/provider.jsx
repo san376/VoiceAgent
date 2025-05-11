ee214d69385f8cb005f49c0ae8bb4a0cbf198559
san376 <155067913+san376@users.noreply.github.com>
2025-05-11 10:56:02 +0530

    sidebar

--- interview/app/provider.jsx
+++ interview/app/provider.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+'use client'
+import { UserDetailContext } from '@/context/UserDetailContext';
+import { supabase } from '@/services/supabaseClient';
+import React, { useContext, useEffect, useState } from 'react'
 
-const provider = ({children}) => {
-  return (
-    <div>
-       <div>{children}</div>
-    </div>
-  )
+const Provider = ({ children }) => {
+
+    const [user, setUser] = useState();
+    useEffect(() => {
+        CreateNewUser();
+    }, [])
+
+    const CreateNewUser = () => {
+        supabase.auth.getUser().then(async ({ data: { user } }) => {
+            // check if user already exists
+            let { data: Users, error } = await supabase
+                .from('Users')
+                .select('*')
+                .eq('email', user?.email);
+
+            console.log(Users)
+
+            if (Users?.length == 0) {
+                const { data, error } = await supabase.from("Users")
+                    .insert([
+                        {
+                            name: user?.user_metadata?.name,
+                            email: user?.email,
+                            picture: user?.user_metadata?.picture
+                        }
+                    ])
+                console.log(data);
+                setUser(data);
+                return;
+            }
+
+            setUser(Users[0]);
+        })
+    }
+    return (
+       <UserDetailContext.Provider value={{user, setUser}}>
+        <div>{children}</div> 
+       </UserDetailContext.Provider>
+    )
 }
 
-export default provider
+export default Provider
+
+export const useUser=()=>{
+    const content = useContext(UserDetailContext);
+    return content;
+}
