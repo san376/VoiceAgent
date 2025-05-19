bdfd8dd92f8faafe8584cce136cb9a58e9c16ceb
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-05-19 15:54:26 +0530

    supabase connected successfully

--- interview/app/(main)/layout.js
+++ interview/app/(main)/layout.js
@@ -3,8 +3,10 @@ import DashboardProvider from "./provider";
 
 function DashboardLayout({ children }) {
   return (
-    <div>
-      <DashboardProvider>{children}</DashboardProvider>
+    <div className="bg-secondary">
+      <DashboardProvider>
+        <div className="p-10">{children}</div>
+      </DashboardProvider>
     </div>
   );
 }
