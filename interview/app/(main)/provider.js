52f5fa934b5c2f86bcf67883adf4a9963aa54b25
san376 <155067913+san376@users.noreply.github.com>
2025-05-20 07:08:47 +0530

    create-interview page

--- interview/app/(main)/provider.js
+++ interview/app/(main)/provider.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./_components/AppSidebar";
+import WelcomeContainer from "./dashboard/_components/WelcomeContainer";
 
 function DashboardProvider({ children }) {
   return (
     <SidebarProvider>
       <AppSidebar/>
-      <div>
-      <SidebarTrigger/>
+      <div className="w-full p-10">
+      {/* <SidebarTrigger/> */}
+      <WelcomeContainer/>
         {children}
       </div>
      
