2054a427cc94f9ed9eb945cb43f9f6ae884cbace
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-05-23 12:08:19 +0530

    Generate Intervie Link

--- interview/app/(main)/dashboard/create-interview/_components/FormContainer.jsx
+++ interview/app/(main)/dashboard/create-interview/_components/FormContainer.jsx
@@ -1,6 +1,6 @@
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Select,
   SelectContent,
@@ -8,21 +8,46 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { InterviewType } from '@/services/Constants'
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
 
-function FormContainer() {
+function FormContainer({onHandleInputChange,GoToNext}) {
+  const [interviewType,setInterviewType]=useState([]);
+
+  useEffect(()=>{
+    if(interviewType){
+      onHandleInputChange('type',interviewType)
+    }
+  },[interviewType])
+
+  const AddInterviewType=(type)=>{
+    const data=interviewType.includes(type);
+    if(!data){
+      setInterviewType(prev=>[...prev,type])
+    }else{
+      const result=interviewType.filter(item=>item!=type);
+      setInterviewType(result);
+    }
+  }
+
   return (
-    <div className='p-5 bg-white'>
+    <div className='p-5 bg-white rounded-xl'>
       <div>
-        <h2 className='text-sm'>Job Position</h2>
-        <Input placeholder="e.g. Full Stack Developer" className='m-2' />
+        <h2 className='text-sm font-medium'>Job Position</h2>
+        <Input placeholder="e.g. Full Stack Developer" className='m-2' 
+        onChange={(event)=>onHandleInputChange('jobPosition',event.target.value)}
+        />
       </div>
       <div className='mt-5'>
         <h2 className='text-sm'>Job Description</h2>
-        <Textarea placeholder="Enter details job description" className='m-2 h-[200px]' />
+        <Textarea placeholder="Enter details job description" className='m-2 h-[200px]' 
+        onChange={(event)=>onHandleInputChange('jobDescription',event.target.value)}
+        />
       </div>
       <div className='mt-5'>
-        <h2 className='text-sm'>Interview Duration</h2>
-        <Select>
+        <h2 className='text-sm font-medium'>Interview Duration</h2>
+        <Select onValueChange={(value)=>onHandleInputChange('duration',value)}>
           <SelectTrigger className="w-full mt-2">
             <SelectValue placeholder="Select Duration" />
           </SelectTrigger>
@@ -34,7 +59,22 @@ function FormContainer() {
             <SelectItem value="60 Min">60 Min</SelectItem>
           </SelectContent>
         </Select>
-
+      </div>
+      <div className='mt-5'>
+        <h2 className='text-sm font-medium'>Interview Type</h2>
+        <div className='flex gap-3 flex-wrap mt-2'>
+          {InterviewType.map((type,index)=>(
+            <div key={index} className={`flex items-center cursor-pointer gap-2 p-1 px-2 bg-white border border-gray-300 rounded-2xl hover:bg-secondary ${interviewType.includes(type.title)&& 'bg-blue-100 text-primary'}`}
+            onClick={()=>AddInterviewType(type.title)}
+            >
+              <type.icon className='h-4 w-4'/>
+              <span>{type.title}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='mt-7 flex justify-end' onClick={()=>GoToNext()}>
+        <Button>Generate Question <ArrowRight/> </Button>
       </div>
     </div>
   )
--- interview/app/(main)/dashboard/create-interview/page.jsx
+++ interview/app/(main)/dashboard/create-interview/page.jsx
@@ -4,10 +4,39 @@ import { ArrowLeft, Lasso } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import FormContainer from './_components/FormContainer';
+import QuestionList from './_components/QuestionList';
+import { toast } from 'sonner';
+import InterviewLink from './_components/InterviewLink';
 
 function CreateInterview() {
     const router = useRouter();
     const [step, setStep]= useState(1);
+    const [formData,setFormData]=useState();
+    const [interviewId,setInterviewId]=useState();
+    const onHandleInputChange=(field,value)=>{
+      setFormData(prev=>({
+        ...prev,
+        [field]:value
+      }))
+      console.log("FormData",formData)
+    }
+
+    const onGoToNext=()=>{
+      if(!formData?.jobPosition || !formData?.jobDescription || !formData?.duration || !formData?.type){
+        toast('please enter all details!')
+        return;
+      }
+      setStep(step+1)
+    }
+
+
+    const onCreateLink=(interview_id)=>{
+      setInterviewId(interview_id);
+      setStep(step+1);
+    }
+
+
+
   return (
     <div className='mt-10 px-10 md:px-24 lg:px-44 xl:px-56'>
         <div className='flex gap-5 items-center'>
@@ -15,7 +44,11 @@ function CreateInterview() {
             <h2 className='font-bold text-2xl'>Create New Interview</h2>
         </div>
         <Progress value={step * 33.33} className='my-5'/>
-        <FormContainer/>
+        {step==1?<FormContainer onHandleInputChange={onHandleInputChange}
+        GoToNext={()=>onGoToNext()}
+        />
+        :step==2?<QuestionList formData={formData} onCreateLink={(interview_id)=>onCreateLink(interview_id)}/>
+        :step == 3 ? <InterviewLink interview_id={interviewId} formData={formData}/>: null}
     </div>
   )
 }
