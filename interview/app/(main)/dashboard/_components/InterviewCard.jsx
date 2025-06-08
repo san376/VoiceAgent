cec69640477901c2323d6a8c3ab993a942527030
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-06-08 17:30:38 +0530

    Dispaly Schedule Interview

--- interview/app/(main)/dashboard/_components/InterviewCard.jsx
+++ interview/app/(main)/dashboard/_components/InterviewCard.jsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { Copy, Send } from 'lucide-react'
+import { ArrowRight, Copy, Send } from 'lucide-react'
 import moment from 'moment'
 import React from 'react'
 import { toast } from 'sonner'
 
-function InterviewCard({ interview }) {
+function InterviewCard({ interview, viewDetail = false }) {
     const url = process.env.NEXT_PUBLIC_HOST_URL + '/' + interview?.interview_id
     const copyLink = () => {
         navigator.clipboard.writeText(url);
@@ -20,11 +20,17 @@ function InterviewCard({ interview }) {
                 <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM Y')}</h2>
             </div>
             <h2 className='mt-3 text-lg font-bold'>{interview?.jobPosition}</h2>
-            <h2 className='mt-2'>{interview?.duration}</h2>
-            <div className='flex  gap-3 w-full mt-5'>
+            <h2 className='mt-2 flex justify-between text-gray-500'>{interview?.duration}
+                <span className='text-green-700'>{interview['interview-feedback']?.length} Candidates</span>
+            </h2>
+                
+            {!viewDetail ? <div className='flex  gap-3 w-full mt-5'>
                 <Button variant='outline' className={'w-1/2'} onClick={copyLink} ><Copy />Copy</Button>
                 <Button className={'w-1/2'} onClick={onSend} ><Send />Send</Button>
             </div>
+                :
+                <Button className="mt-5 w-full" variant="outline">View Detail <ArrowRight/></Button>
+            }
         </div>
     )
 }
---
+++ interview/app/(main)/scheduled-interview/page.jsx
@@ -0,0 +1,49 @@
+'use client'
+import { useUser } from '@/app/provider'
+import { Button } from '@/components/ui/button';
+import { supabase } from '@/services/supabaseClient'
+import { Video } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import InterviewCard from '../dashboard/_components/InterviewCard';
+
+function ScheduledInterview() {
+    const {user}=useUser();
+    const [interviewList, setInterviewList]=useState();
+
+    useEffect(()=>{
+        user && GetInterviewList();
+    },[user])
+
+    const GetInterviewList = async() => {
+        const result = await supabase.from('Interviews')
+            .select('jobPosition,duration,interview_id,interview-feedback(userEmail)')
+            .eq('userEmail', user?.email)
+            .order('id', { ascending: false })
+        console.log(result);    
+        setInterviewList(result.data);
+    }
+
+    return (
+        <div className='mt-5'>
+           <h2 className='font-bold text-xl'>Interview List with Candidate Feedback</h2>
+            {interviewList?.length ==0 &&
+           <div className='p-5 flex flex-col gap-3 items-center mt-5'>
+            <Video className='h-10 w-10 text-primary'/>
+            <h2>You don't have any interview created!</h2>
+            <Button>+ Create New Interview</Button>
+           </div>
+          }
+          {interviewList&&
+             <div className='grid grid-cols-2 mt-5 xl:grid-cols-3 gap-5'>
+               {interviewList?.map((interview,index)=>(
+                <InterviewCard interview={interview} key={index} viewDetail={true}/>
+               ))}
+             </div>
+
+           }
+        </div>
+    )
+}
+
+export default ScheduledInterview
+ScheduledInterview
