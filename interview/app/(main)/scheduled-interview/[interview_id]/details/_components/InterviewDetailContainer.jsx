75f8db3ab864e561f9cc9647a1929fa174ef04a1
san376 <155067913+san376@users.noreply.github.com>
2025-08-15 02:05:24 +0530

    Scheduled Interview with Interview Details

--- interview/app/(main)/scheduled-interview/[interview_id]/details/_components/InterviewDetailContainer.jsx
+++ interview/app/(main)/scheduled-interview/[interview_id]/details/_components/InterviewDetailContainer.jsx
@@ -1,3 +1,57 @@
+// import { Calendar, Clock } from 'lucide-react'
+// import moment from 'moment'
+// import React from 'react'
+
+// function InterviewDetailContainer({ interviewDetail }) {
+//   return (
+//     <div className='p-5 bg-white rounded-lg mt-5'>
+
+//       <h2>Fullstack{interviewDetail?.jobPosition}</h2>
+//       <div className='mt-5 flex items-center justify-between lg:pr-52'>
+//         <div>
+//           <h2 className='text-xs text-gray-500'>Duration</h2>
+//           <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />5 Min{interviewDetail?.duration}</h2>
+//         </div>
+//         <div>
+//           <h2 className='text-xs text-gray-500'>Created On</h2>
+//           <h2 className='flex text-sm font-bold items-center gap-2'><Calendar className='h-4 w-4' />{moment(interviewDetail?.created_at).format('MMM DD, YYYY')}</h2>
+//         </div>
+//         <div>
+//           <h2 className='text-xs text-gray-500'>Type</h2>
+//           {/* <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4'/>Technical{JSON.parse(interviewDetail?.type)[0]}</h2> */}
+//           <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />Technical{interviewDetail?.type[0]}</h2>
+//         </div>
+
+//       </div>
+//       <div className='mt-5'>
+//         <h2 className='font-bold'>Job Description</h2>
+//         <p className='text-sm'>Design, develop, and maintain scalable web applications using both frontend and backend technologies, including React.js, Node.js, and databases like MongoDB or PostgreSQL.
+
+//           Collaborate with cross-functional teams to deliver end-to-end solutions, ensuring seamless integration of UI/UX, server-side logic, APIs, and cloud deployment.{interviewDetail?.jobDescription}</p>
+//       </div>
+//       <div>
+//         <h2 className='font-bold mt-2'>Interview Questions</h2>
+//         <div className='grid grid-cols-2 gap-5'>
+//           {interviewDetail?.questionList.map((item, index) => (
+//             <h2 className='text-xs'>{index+1}.{item?.question}</h2>
+//           ))}
+//         </div>
+//          <div className='text-sm '>
+//             <p>1. Can you describe a full stack project you’ve built recently?</p>
+//           <p>2. What are the biggest challenges in full stack development?</p>
+//           <p>3. How do you ensure smooth communication between frontend and backend?</p>
+//           <p>4. What are React hooks, and how do you use useEffect?</p>
+//           <p>5. How do you manage state in large React applications?</p>
+//           <p>6. Explain the difference between controlled and uncontrolled components.</p>
+//           </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default InterviewDetailContainer
+
+
 import { Calendar, Clock } from 'lucide-react'
 import moment from 'moment'
 import React from 'react'
@@ -10,7 +64,7 @@ function InterviewDetailContainer({ interviewDetail }) {
       <div className='mt-5 flex items-center justify-between lg:pr-52'>
         <div>
           <h2 className='text-xs text-gray-500'>Duration</h2>
-          <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />5 Min{interviewDetail?.duration}</h2>
+          <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />{interviewDetail?.duration}</h2>
         </div>
         <div>
           <h2 className='text-xs text-gray-500'>Created On</h2>
@@ -19,31 +73,22 @@ function InterviewDetailContainer({ interviewDetail }) {
         <div>
           <h2 className='text-xs text-gray-500'>Type</h2>
           {/* <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4'/>Technical{JSON.parse(interviewDetail?.type)[0]}</h2> */}
-          <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />Technical{interviewDetail?.type[0]}</h2>
+          <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />{interviewDetail?.type[0]}</h2>
         </div>
 
       </div>
       <div className='mt-5'>
         <h2 className='font-bold'>Job Description</h2>
-        <p className='text-sm'>Design, develop, and maintain scalable web applications using both frontend and backend technologies, including React.js, Node.js, and databases like MongoDB or PostgreSQL.
-
-          Collaborate with cross-functional teams to deliver end-to-end solutions, ensuring seamless integration of UI/UX, server-side logic, APIs, and cloud deployment.{interviewDetail?.jobDescription}</p>
+        <p className='text-sm'>{interviewDetail?.jobDescription}</p>
       </div>
       <div>
         <h2 className='font-bold mt-2'>Interview Questions</h2>
         <div className='grid grid-cols-2 gap-5'>
           {interviewDetail?.questionList.map((item, index) => (
-            <h2 className='text-xs'>{index+1}.{item?.question}</h2>
+            <h2 key={index} className='text-xs'>{index+1}.{item?.question}</h2>
           ))}
+         
         </div>
-         <div className='text-sm '>
-            <p>1. Can you describe a full stack project you’ve built recently?</p>
-          <p>2. What are the biggest challenges in full stack development?</p>
-          <p>3. How do you ensure smooth communication between frontend and backend?</p>
-          <p>4. What are React hooks, and how do you use useEffect?</p>
-          <p>5. How do you manage state in large React applications?</p>
-          <p>6. Explain the difference between controlled and uncontrolled components.</p>
-          </div>
       </div>
     </div>
   )
--- interview/app/(main)/scheduled-interview/[interview_id]/details/page.jsx
+++ interview/app/(main)/scheduled-interview/[interview_id]/details/page.jsx
@@ -1,3 +1,47 @@
+// 'use client'
+// import { useUser } from '@/app/provider';
+// import { useParams } from 'next/navigation'
+// import React, { use, useEffect, useState } from 'react'
+// import InterviewDetailContainer from './_components/InterviewDetailContainer';
+// import { supabase } from '@/services/supabaseClient';
+// import CandidateList from './_components/CandidateList';
+
+// function InterviewDetail() {
+//     const { interview_id } = useParams();
+//     const {user} = useUser();
+//     const [interviewDetail, setInterviewDetail]=useState();
+
+//     useEffect(()=>{
+//         user && GetInterviewDetail();
+//     },[user])
+
+//     const GetInterviewDetail = async () => {
+//         const result = await supabase.from('Interviews')
+//             .select(`jobPosition,jobDescription,type,questionList,duration,created_at,
+//                 interview_id,interview-feedback(userEmail,useName,feedback,created_at)`)
+//             .eq('userEmail', user?.email)
+//             .eq('interview_id',interview_id)
+
+//             setInterviewDetail(result?.data[0]);
+//             //setInterviewDetail(result);
+//         console.log(result);    
+//     }
+
+    
+
+//     return (
+//         <div className='mt-5'>
+//            <h2 className='font-bold text-2xl'>Interview Detail</h2>
+//            <h2></h2>
+//            <InterviewDetailContainer interviewDetail={interviewDetail}/>
+//            <CandidateList candidateList={interviewDetail?.['interview-feedback']}/>
+//         </div>
+//     )
+// }
+
+// export default InterviewDetail
+
+
 'use client'
 import { useUser } from '@/app/provider';
 import { useParams } from 'next/navigation'
@@ -10,22 +54,50 @@ function InterviewDetail() {
     const { interview_id } = useParams();
     const {user} = useUser();
     const [interviewDetail, setInterviewDetail]=useState();
-
+    console.log(5)
+    console.log(user)
+    console.log(4)
+    
     useEffect(()=>{
         user && GetInterviewDetail();
     },[user])
 
-    const GetInterviewDetail = async () => {
-        const result = await supabase.from('Interviews')
-            .select(`jobPosition,jobDescription,type,questionList,duration,created_at,
-                interview_id,interview-feedback(userEmail,useName,feedback,created_at)`)
-            .eq('userEmail', user?.email)
-            .eq('interview_id',interview_id)
+    // const GetInterviewDetail = async () => {
+    //     console.log(3)
+    //     const result = await supabase.from('Interviews')
+    //         .select(`jobPosition,jobDescription,type,questionList,duration,created_at,
+    //             interview_id,interview-feedback(userEmail,useName,feedback,created_at)`)
+    //         .eq('userEmail', user?.email)
+    //         .eq('interview_id',interview_id)
 
-            setInterviewDetail(result?.data[0]);
-            //setInterviewDetail(result);
-        console.log(result);    
-    }
+    //         setInterviewDetail(result?.data[0]);
+    //         //setInterviewDetail(result);
+         
+    //     console.log(result);    
+    // }
+    const GetInterviewDetail = async () => {
+  console.log(3);
+  const result = await supabase
+    .from('Interviews')
+    .select(`
+      jobPosition, jobDescription, type, questionList, duration, created_at,
+      interview_id, interview-feedback(userEmail, userName, feedback, created_at)
+    `)
+    .eq('userEmail', user?.email)
+    .eq('interview_id', String(interview_id)); // ensure correct type
+
+  if (result.error) {
+    console.error('Supabase error:', result.error);
+    return;
+  }
+
+  if (result.data?.length) {
+    setInterviewDetail(result.data[0]);
+  } else {
+    console.warn('No data returned for interview_id:', interview_id);
+  }
+};
+
 
     
 
