6225eeb5504587aa49621d347e17ff0145892ca9
Sanjay Palegar <sanjpalegar02@gmail.com>
2025-10-17 10:27:57 +0530

    check

--- interview/app/provider.jsx
+++ interview/app/provider.jsx
@@ -84,6 +84,7 @@ const Provider = ({ children }) => {
                             picture: user?.user_metadata?.picture
                         }
                     ])
+                    
                 console.log(data);
                 setUser(data);
                 return;
